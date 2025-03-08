9521cac0de444d21879285e8762d3de03be4a19a
Archit Gupta <architgupta539@gmail.com>
2025-03-09 05:11:24 +0530

    eslint true

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds:true,
+  },
 };
 
 export default nextConfig;
